cf126ef7f74a57e2c843f1d32185dc6c6ed213c6
Nick Jones <njhazelh@gmail.com>
2015-09-13 11:59:16 -0700

    Add some files and the initial install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install zsh vim terminator curl xclip
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp .zshrc ~
+source ~/.zshrc
+cp .vimrc ~
+cp terminator-config ~/.config/terminator/config
+
+# Git Configuration
+ssh-keygen -t rsa -b 4096 -C "njhazelh@gmail.com"
+git config --global core.editor "vim"
+git config --global push.default
+git config --global user.name "Nick Jones"
+git config --global user.email "njhazelh@gmail.com"
+
+# Install nvm (NodeJS version Manager)
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
+nvm install stable
+
+sudo apt-get install openjdk-8-jdk
+
+#Check to install docker
